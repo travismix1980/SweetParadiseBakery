ee00e7a08f36bae503eaf593679c057c0e051d6d
Travis Mix <travismix1980@gmail.com>
2023-09-04 09:27:52 -0700

    Controller Done

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SweetParadiseBakery.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Models.Bakery bakery1 = new()
+            {
+                Id = 1,
+                Name = "Sweet Paradise Bakery",
+                Address = "1 Paradise Rd.",
+                BakeryValue = 200000.00M,
+                FoundingDate = new DateTime(2017, 8, 2)
+            };
+
+            return View(bakery1);
+        }
+    }
+}
