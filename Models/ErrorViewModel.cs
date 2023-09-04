5ae0f6876eac341e43037561d95e3e0b8a18e7a0
Travis Mix <travismix1980@gmail.com>
2023-09-04 09:16:58 -0700

    removed default Models, Views, and Controllers

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SweetParadiseBakery.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
