916a8c451bf21b128be358a6f83e7617ad932a2e
Travis Mix <travismix1980@gmail.com>
2023-09-04 09:22:10 -0700

    Model Complete

---
+++ Models/Bakery.cs
@@ -0,0 +1,11 @@
+﻿namespace SweetParadiseBakery.Models
+{
+    public class Bakery
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public DateTime FoundingDate { get; set; }
+        public decimal BakeryValue { get; set; }
+    }
+}
