41fb77387669c52e95ed69275a600a55bda90969
Travis Mix <travismix1980@gmail.com>
2023-09-04 09:15:43 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SweetParadiseBakery.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
